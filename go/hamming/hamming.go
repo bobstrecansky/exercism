de50774ae907a31b55a797443bc08ead41b8be42
Bob Strecansky <bob.strecansky@gmail.com>
2022-01-31 14:56:58 -0500

    Update README.md and two exercises

---
+++ go/hamming/hamming.go
@@ -0,0 +1,17 @@
+package hamming
+
+import "errors"
+
+// Distance calculates the Hamming Distance between two DNA strands.
+func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return -1, errors.New("Len(a) != Len(b)")
+	}
+	var count = 0
+	for i := 0; i < len(a); i++ {
+		if string(a[i]) != string(b[i]) {
+			count++
+		}
+	}
+	return count, nil
+}
---
+++ go/raindrops/raindrops.go
@@ -0,0 +1,23 @@
+package raindrops
+
+import (
+	"strconv"
+)
+
+// Convert - AKA Why did they try and cover up FizzBuzz?
+func Convert(in int) (out string) {
+	returnString := ""
+	if in%3 == 0 {
+		returnString += "Pling"
+	}
+	if in%5 == 0 {
+		returnString += "Plang"
+	}
+	if in%7 == 0 {
+		returnString += "Plong"
+	}
+	if returnString == "" {
+		returnString = strconv.Itoa(in)
+	}
+	return returnString
+}
